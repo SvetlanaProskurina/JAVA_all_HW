9657b5d43543cffc3410fe4b69d0653cbb6cc37d
Svetlana Proskurina <amourka@yandex.ru>
2022-12-21 05:55:09 +0300

    format

--- HW6_java/Main.java
+++ HW6_java/Main.java
@@ -10,179 +10,180 @@
 Далее нужно запросить минимальные значения для указанных критериев - сохранить параметры фильтрации можно также в Map.
 Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.
 **/
+
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
 
-        Notebook notebook1 = new Notebook();
-        notebook1.ID = 1;
-        notebook1.maker = "Dell";
-        notebook1.color= "black";
-        notebook1.disk = 512;
-        notebook1.frequency = 1.5;
-        notebook1.RAM = 8;
-        notebook1.OS = "Windows";
-        notebook1.price = 45000;
+  public static void main(String[] args) {
 
-        Notebook notebook2 = new Notebook();
-        notebook2.ID = 2;
-        notebook2.maker= "Acer";
-        notebook2.color= "black";
-        notebook2.disk = 512;
-        notebook2.frequency = 2.7;
-        notebook2.RAM = 16;
-        notebook2.OS = "Windows";
-        notebook2.price = 75000;
+    Notebook notebook1 = new Notebook();
+    notebook1.ID = 1;
+    notebook1.maker = "Dell";
+    notebook1.color = "black";
+    notebook1.disk = 512;
+    notebook1.frequency = 1.5;
+    notebook1.RAM = 8;
+    notebook1.OS = "Windows";
+    notebook1.price = 45000;
 
+    Notebook notebook2 = new Notebook();
+    notebook2.ID = 2;
+    notebook2.maker = "Acer";
+    notebook2.color = "black";
+    notebook2.disk = 512;
+    notebook2.frequency = 2.7;
+    notebook2.RAM = 16;
+    notebook2.OS = "Windows";
+    notebook2.price = 75000;
 
-        Notebook notebook3 = new Notebook();
-        notebook3.ID = 3;
-        notebook3.maker = "Lenovo";
-        notebook3.color= "grey";
-        notebook3.disk = 256;
-        notebook3.frequency = 1.5;
-        notebook3.RAM = 8;
-        notebook3.OS = "Linux";
-        notebook3.price = 35000;
+    Notebook notebook3 = new Notebook();
+    notebook3.ID = 3;
+    notebook3.maker = "Lenovo";
+    notebook3.color = "grey";
+    notebook3.disk = 256;
+    notebook3.frequency = 1.5;
+    notebook3.RAM = 8;
+    notebook3.OS = "Linux";
+    notebook3.price = 35000;
 
+    Notebook notebook4 = new Notebook();
+    notebook4.ID = 4;
+    notebook4.maker = "Apple";
+    notebook4.color = "white";
+    notebook4.disk = 512;
+    notebook4.frequency = 2.8;
+    notebook4.RAM = 16;
+    notebook4.OS = "MacOS";
+    notebook4.price = 95000;
 
-        Notebook notebook4 = new Notebook();
-        notebook4.ID = 4;
-        notebook4.maker = "Apple";
-        notebook4.color= "white";
-        notebook4.disk = 512;
-        notebook4.frequency = 2.8;
-        notebook4.RAM = 16;
-        notebook4.OS = "MacOS";
-        notebook4.price = 95000;
+    ArrayList<Notebook> notebookArrayList = new ArrayList<>();
+    notebookArrayList.add(notebook1);
+    notebookArrayList.add(notebook2);
+    notebookArrayList.add(notebook3);
+    notebookArrayList.add(notebook4);
 
-        ArrayList<Notebook> notebookArrayList = new ArrayList<>();
-        notebookArrayList.add(notebook1);
-        notebookArrayList.add(notebook2);
-        notebookArrayList.add(notebook3);
-        notebookArrayList.add(notebook4);
+    searching(notebookArrayList, request());
+  }
 
-        searching(notebookArrayList, request());
-    }
-
-    private static HashMap<String, String> request() {
-        Scanner scans = new Scanner(System.in);
-        HashMap<String, String> map = new HashMap<>();
+  private static HashMap<String, String> request() {
+    Scanner scans = new Scanner(System.in);
+    HashMap<String, String> map = new HashMap<>();
 // инициализация map
 
+    String inscan;
 
-        String inscan;
+    System.out.println("Введите цифру, или несколько через запятую," +
+        "соответствующих необходимым критериям фильтрации:  " +
+        "1 - Производитель, " +
+        "2 - Операционная система, " +
+        "3 - Оперативная память RAM, " +
+        "4 - Объем жесткого диска, " +
+        "5 - Частота процессора, " +
+        "6 - цвет, " +
+        "7 - стоимость");
+    String[] input = scans.nextLine().split(",");
 
-            System.out.println("Введите цифру, или несколько через запятую," +
-                    "соответствующих необходимым критериям фильтрации:  " +
-                    "1 - Производитель, " +
-                    "2 - Операционная система, " +
-                    "3 - Оперативная память RAM, " +
-                    "4 - Объем жесткого диска, " +
-                    "5 - Частота процессора, " +
-                    "6 - цвет, " +
-                    "7 - стоимость");
-            String[] input = scans.nextLine().split(",");
-
-
-                for (String s : input) {
-                    if (s.equalsIgnoreCase("1")) {
-                        System.out.println("Введите искомое значение параметра: производитель:  ");
-                        inscan = scans.nextLine();
-                        if (inscan != "")
-                            map.put("maker", inscan);
-                    }
-
-                    if (s.equalsIgnoreCase("2")) {
-                        System.out.println("Введите искомое значение параметра: операционная система:  ");
-                        inscan = scans.nextLine();
-                        if (inscan != "")
-                            map.put("OS", inscan);
-                    }
-
-                    if (s.equalsIgnoreCase("3")) {
-                        System.out.println("Введите искомое значение параметра: Оперативная память RAM  ");
-                        inscan = scans.nextLine();
-                        if (inscan != "")
-                            map.put("RAM", inscan);
-                    }
-
-                    if (s.equalsIgnoreCase("4")) {
-                        System.out.println("Введите искомое значение параметра: объем диска ");
-                        inscan = scans.nextLine();
-                        if (inscan != "")
-                            map.put("disk", inscan);
-                    }
-
-                    if (s.equalsIgnoreCase("5")) {
-                        System.out.println("Введите искомое значение параметра: частота процессора ");
-                        inscan = scans.nextLine();
-                        if (inscan != "")
-                            map.put("frequency", inscan);
-                    }
-
-                    if (s.equalsIgnoreCase("6")) {
-                        System.out.println("Введите искомое значение параметра: цвет  ");
-                        inscan = scans.nextLine();
-                        if (inscan != "")
-                            map.put("color", inscan);
-                    }
-
-                    if (s.equalsIgnoreCase("7")) {
-                        System.out.println("Введите искомое значение параметра: стоимость ");
-                        inscan = scans.nextLine();
-                        if (inscan != "")
-                            map.put("price", inscan);
-                    }
-                    if (s.equalsIgnoreCase("")) {
-                        System.out.println("Ничего не выбрано.");
-                        // можем предложить по умолчанию или рекламный продукт
-   /*                 map.put("maker", "Dell");
-                    map.put("OS", "Windows");
-                    map.put("RAM", "8");*/
-                    }
-                }
-        scans.close();
-        return map;
-    }
-    public static void searching(ArrayList<Notebook> listObject, HashMap<String,String> request) {
-        ArrayList<Notebook> out = new ArrayList<>();
-        if(request.isEmpty()){
-            System.out.println("Для поиска необходим хотя бы один параметр \n");
-
-        } else {
-            var params = request.keySet();
-            for (Object param: params){
-                for (Notebook nb:listObject){
-
-                    if (nb.parametrsMap().get(param.toString()).toString().equalsIgnoreCase(request.get(param.toString()))){
-                        if ( !out.contains(nb)){
-                            out.add(nb);
-                        }
-
-                    }
-                }
-            }
-            for (Object param: params){
-                ArrayList<Notebook> result_out = new ArrayList<>(out);
-                for (Notebook nb: result_out){
-                    if (!nb.parametrsMap().get(param.toString()).toString().equalsIgnoreCase(request.get(param.toString()))){
-                        out.remove(nb);
-                    }
-                }
-            }
-            if (out.isEmpty())
-            {
-                System.out.println("Ничего не найдено");
-
-            } else {
-                for (Notebook nb: out){
-                    System.out.println("Можем предложить: ");
-                    System.out.println(nb);
-                }
-            }
+    for (String s : input) {
+      if (s.equalsIgnoreCase("1")) {
+        System.out.println("Введите искомое значение параметра: производитель:  ");
+        inscan = scans.nextLine();
+        if (inscan != "") {
+          map.put("maker", inscan);
         }
+      }
 
+      if (s.equalsIgnoreCase("2")) {
+        System.out.println("Введите искомое значение параметра: операционная система:  ");
+        inscan = scans.nextLine();
+        if (inscan != "") {
+          map.put("OS", inscan);
+        }
+      }
+
+      if (s.equalsIgnoreCase("3")) {
+        System.out.println("Введите искомое значение параметра: Оперативная память RAM  ");
+        inscan = scans.nextLine();
+        if (inscan != "") {
+          map.put("RAM", inscan);
+        }
+      }
+
+      if (s.equalsIgnoreCase("4")) {
+        System.out.println("Введите искомое значение параметра: объем диска ");
+        inscan = scans.nextLine();
+        if (inscan != "") {
+          map.put("disk", inscan);
+        }
+      }
+
+      if (s.equalsIgnoreCase("5")) {
+        System.out.println("Введите искомое значение параметра: частота процессора ");
+        inscan = scans.nextLine();
+        if (inscan != "") {
+          map.put("frequency", inscan);
+        }
+      }
+
+      if (s.equalsIgnoreCase("6")) {
+        System.out.println("Введите искомое значение параметра: цвет  ");
+        inscan = scans.nextLine();
+        if (inscan != "") {
+          map.put("color", inscan);
+        }
+      }
+
+      if (s.equalsIgnoreCase("7")) {
+        System.out.println("Введите искомое значение параметра: стоимость ");
+        inscan = scans.nextLine();
+        if (inscan != "") {
+          map.put("price", inscan);
+        }
+      }
+      if (s.equalsIgnoreCase("")) {
+        System.out.println("Ничего не выбрано.");
+      }
     }
+    scans.close();
+    return map;
+  }
+
+  public static void searching(ArrayList<Notebook> listObject, HashMap<String, String> request) {
+    ArrayList<Notebook> out = new ArrayList<>();
+    if (request.isEmpty()) {
+      System.out.println("Для поиска необходим хотя бы один параметр \n");
+
+    } else {
+      var params = request.keySet();
+      for (Object param : params) {
+        for (Notebook nb : listObject) {
+
+          if (nb.parametrsMap().get(param.toString()).toString()
+              .equalsIgnoreCase(request.get(param.toString()))) {
+            if (!out.contains(nb)) {
+              out.add(nb);
+            }
+          }
+        }
+      }
+      for (Object param : params) {
+        ArrayList<Notebook> result_out = new ArrayList<>(out);
+        for (Notebook nb : result_out) {
+          if (!nb.parametrsMap().get(param.toString()).toString()
+              .equalsIgnoreCase(request.get(param.toString()))) {
+            out.remove(nb);
+          }
+        }
+      }
+      if (out.isEmpty()) {
+        System.out.println("Ничего не найдено");
+
+      } else {
+        for (Notebook nb : out) {
+          System.out.println("Можем предложить: ");
+          System.out.println(nb);
+        }
+      }
+    }
+  }
 }
--- HW6_java/Notebook.java
+++ HW6_java/Notebook.java
@@ -6,71 +6,82 @@ import java.util.Random;
  * Класс описывает ноутбуки
  */
 public class Notebook {
-    String maker;       // производитель
-    String color;       // цвет
-    String OS;          // операционная система
-    int disk;            // Диск
-    int RAM;            // оперативка
-    double frequency;      // частота
-    int price;
-    int ID; // ID ноутбука
 
-    /**
-     * toString для экземпляра класса Notebook
-     * @return String.format...
-     */
-    @Override
-    public String toString() {
-        return String.format("%s, %s, %s, %s, %s, %s, %s, %s",ID, maker, disk, RAM, frequency,OS, color, price);
-    }
+  String maker;       // производитель
+  String color;       // цвет
+  String OS;          // операционная система
+  int disk;            // Диск
+  int RAM;            // оперативка
+  double frequency;      // частота
+  int price;
+  int ID; // ID ноутбука
 
-    public  Map<Object,Object> parametrsMap() {
-        Map<Object,Object> parametrsNotebook = new HashMap<>();
-        parametrsNotebook.put("ID", ID);
-        parametrsNotebook.put("maker", maker);
-        parametrsNotebook.put("color", color);
-        parametrsNotebook.put("OS", OS);
-        parametrsNotebook.put("disk", disk);
-        parametrsNotebook.put("RAM", RAM);
-        parametrsNotebook.put("frequency", frequency);
-        parametrsNotebook.put("price", price);
+  /**
+   * toString для экземпляра класса Notebook
+   *
+   * @return String.format...
+   */
+  @Override
+  public String toString() {
+    return String.format("%s, %s, %s, %s, %s, %s, %s, %s", ID, maker, disk, RAM, frequency, OS,
+        color, price);
+  }
 
-        return parametrsNotebook;
-    }
-    /**
-     * Setter для экземпляра класса ноутбук. заполняет или изменяет созданный ранее экземпляр.
-     * @param IDs - ИД ноутбука
-     * @param makers - Брэнд производителя ноутбука
-     * @param RAMs - размер оперативной памяти в ГБ.
-     * @param disks - объём жёсткого диска в ГБ.
-     * @param OSs - операционная система
-     * @param colors - цвет корпуса ноутбука
-     * @param frequencys - частота  ghjwtccjhf (ГГц)
-     * @param prices - цена в рублях.
-     */
-    public void setParams(int disks,
-                          int IDs,
-                          int prices,
-                          int RAMs,
-                          double frequencys,
-                          String makers,
-                          String OSs,
-                          String colors)
-    {
-        if(IDs > 0) ID = IDs;
-        else {
-            Random rnd = new Random();
-            ID = rnd.nextInt(1000, 100000);
-        }
-        if(RAMs > 0)RAM = RAMs;
-        else RAM = 0;
-        if(disks > 0)disk = disks;
-        else disk = 0;
-        maker = makers;
-        OS = OSs;
-        color = colors;
-        frequency = frequencys;
-        price = prices;
+  public Map<Object, Object> parametrsMap() {
+    Map<Object, Object> parametrsNotebook = new HashMap<>();
+    parametrsNotebook.put("ID", ID);
+    parametrsNotebook.put("maker", maker);
+    parametrsNotebook.put("color", color);
+    parametrsNotebook.put("OS", OS);
+    parametrsNotebook.put("disk", disk);
+    parametrsNotebook.put("RAM", RAM);
+    parametrsNotebook.put("frequency", frequency);
+    parametrsNotebook.put("price", price);
 
-    }
+    return parametrsNotebook;
+  }
+
+  /**
+   * Setter для экземпляра класса ноутбук. заполняет или изменяет созданный ранее экземпляр.
+   *
+   * @param IDs        - ИД ноутбука
+   * @param makers     - Брэнд производителя ноутбука
+   * @param RAMs       - размер оперативной памяти в ГБ.
+   * @param disks      - объём жёсткого диска в ГБ.
+   * @param OSs        - операционная система
+   * @param colors     - цвет корпуса ноутбука
+   * @param frequencys - частота  ghjwtccjhf (ГГц)
+   * @param prices     - цена в рублях.
+   */
+  public void setParams(int disks,
+      int IDs,
+      int prices,
+      int RAMs,
+      double frequencys,
+      String makers,
+      String OSs,
+      String colors) {
+      if (IDs > 0) {
+          ID = IDs;
+      } else {
+          Random rnd = new Random();
+          ID = rnd.nextInt(1000, 100000);
+      }
+      if (RAMs > 0) {
+          RAM = RAMs;
+      } else {
+          RAM = 0;
+      }
+      if (disks > 0) {
+          disk = disks;
+      } else {
+          disk = 0;
+      }
+    maker = makers;
+    OS = OSs;
+    color = colors;
+    frequency = frequencys;
+    price = prices;
+
+  }
 }
